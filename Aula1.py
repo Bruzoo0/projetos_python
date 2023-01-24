18bb415d8d2abd0fa820c1b64fcc128b4aa92d77
bruzoo0 <brunosouzabs.bs4@gmail.com>
2023-01-24 17:28:34 -0300

    arendendo função print

---
+++ Aula1.py
@@ -0,0 +1,16 @@
+# Não vai ser lido 
+print(123),
+print(456),
+
+"""
+Essa é uma maneira de eu escrever comentários 
+sobre o código que estou escrevendo.
+Esse sistema de tres 'aspas' duplas...
+"""
+
+'''
+...ou únicas são chamadas de DocString 
+e apesar de não serem exibidas 
+elas são lidas pelo interpretador 
+do Python.
+'''
---
+++ Aula2.py
@@ -0,0 +1,21 @@
+print(12, 34, sep='', end='#') 
+print(56, 78, sep="", end="\n")
+print('teste')
+"""
+Para quebra de linha no Windows 
+end=\r\n = CLRF
+Para quebra de linha no Linux, mac, etc 
+end=/n
+
+funcão sep= definir o separador
+que pode ser qualquer coisa
+desde uma palavra até um número 
+ou uma '#' 
+
+Sempre que for 'primtar palavras é preciso definir o argumento com ' '
+"""
+'''
+Quando você comete e executa um erro no python, 
+no próprio terminal já avisa onde você errou
+
+'''
